bd7400ebbfb026ce18f3ab60151d3c19a596fb93
Ben Morse <bhmorse@gmail.com>
2018-06-07 15:28:11 -0700

    add more fields to json objects

--- houndify/server_response.go
+++ houndify/server_response.go
@@ -18,31 +18,55 @@ type HoundifyDisambiguation struct {
 	ChoiceData []HoundifyDisambiguationChoice
 }
 
+type HoundifyBuildInfo struct {
+	User        string
+	Date        string
+	Machine     string
+	SVNRevision string
+	SVNBranch   string
+	BuildNumber string
+	King        string
+	Variant     string
+}
+
+type HoundifyDomain struct {
+	Domain         string
+	DomainUniqueId string
+	CreditsUsed    float64
+}
+
 type HoundifyResponseResult struct {
 	CommandKind               string
 	SpokenResponse            string
 	SpokenResponseLong        string
 	WrittenResponse           string
 	WrittenResponseLong       string
-	SpokenResponseSSML        *string   `json:"omitempty"`
-	SpokenResponseSSMLLong    *string   `json:"omitempty"`
-	SmallScreenHTML           *string   `json:"omitempty"`
-	LargeScreenHTML           *string   `json:"omitempty"`
-	UnderstandingConfidence   *float64  `json:"omitempty"`
-	OutputOverrideDiagnostics *[]string `json:"omitempty"`
+	SpokenResponseSSML        *string   `json:"SpokenResponseSSML,omitempty"`
+	SpokenResponseSSMLLong    *string   `json:"SpokenResponseSSMLLong,omitempty"`
+	SmallScreenHTML           *string   `json:"SmallScreenHTML,omitempty"`
+	LargeScreenHTML           *string   `json:"LargeScreenHTML,omitempty"`
+	UnderstandingConfidence   *float64  `json:"UnderstandingConfidence,omitempty"`
+	OutputOverrideDiagnostics *[]string `json:"OutputOverrideDiagnostics,omitempty"`
 	AutoListen                bool
 	ConversationState         interface{}
 }
 
 type HoundifyResponse struct {
-	Format          string
-	FormatVersion   string
-	Status          string
-	NumToReturn     int64
-	AllResults      []HoundifyResponseResult
-	ErrorMessage    *string                 `json:"ErrorMessage,omitempty"`
-	ResultsAreFinal *[]bool                 `json:"ResultsAreFinal,omitempty"`
-	Disambiguation  *HoundifyDisambiguation `json:"Disambiguation,omitempty"`
+	Format            string
+	FormatVersion     string
+	Status            string
+	NumToReturn       int64
+	AllResults        []HoundifyResponseResult
+	ErrorMessage      *string                 `json:"ErrorMessage,omitempty"`
+	ResultsAreFinal   *[]bool                 `json:"ResultsAreFinal,omitempty"`
+	Disambiguation    *HoundifyDisambiguation `json:"Disambiguation,omitempty"`
+	QueryID           *string                 `json:"QueryID,omitempty"`
+	ServerGeneratedId *string                 `json:"ServerGeneratedId,omitempty"`
+	AudioLength       *float64                `json:"AudioLength,omitempty"`
+	RealSpeechTime    *float64                `json:"RealSpeechTime,omitempty"`
+	RealTime          *float64                `json:"RealTime,omitempty"`
+	BuildInfo         HoundifyBuildInfo
+	DomainUsage       []HoundifyDomain
 }
 
 // ParseWrittenResponse will take final server response JSON (as a string)
